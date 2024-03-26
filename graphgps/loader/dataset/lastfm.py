750b2b322a485eca7b10e115df8449bd84ee72b4
Florian Grötschla <fgroetschla@ethz.ch>
2024-03-26 21:26:32 +0100

    Simplify dataloader and add example config

--- graphgps/loader/dataset/lastfm.py
+++ graphgps/loader/dataset/lastfm.py
@@ -41,7 +41,7 @@ class LastFM(InMemoryDataset):
         force_reload (bool, optional): Whether to re-process the dataset.
             (default: :obj:`False`)
     """
-    url = 'https://www.dropbox.com/s/jvlbs09pz6zwcka/LastFM_processed.zip?dl=1'
+    url = 'https://polybox.ethz.ch/index.php/s/EeQrarTtTXz1fv1/download'
     
     url_embeddings = 'https://polybox.ethz.ch/index.php/s/dkQW1wLywyT1NuV'
 
@@ -78,13 +78,12 @@ class LastFM(InMemoryDataset):
     @property
     def raw_file_names(self) -> List[str]:
         return [
-            'adjM.npz', 'node_types.npy', 'train_val_test_neg_user_artist.npz',
-            'train_val_test_pos_user_artist.npz'
+            'data.pth'
         ]
 
     @property
     def processed_file_names(self) -> str:
-        return 'data.pt'
+        return 'data.pth'
 
     def download(self) -> None:
         path = download_url(self.url, self.raw_dir)
@@ -95,112 +94,36 @@ class LastFM(InMemoryDataset):
         data = Data()
         node_nums = {}
 
-        new_data = torch.load('data.pth')
-
-        node_type_idx = np.load(osp.join(self.raw_dir, 'node_types.npy'))
-        node_type_idx = torch.from_numpy(node_type_idx).to(torch.long)
-
-        node_types = ['user', 'artist', 'tag']
-        for i, node_type in enumerate(node_types):
-            node_nums[node_type] = int((node_type_idx == i).sum())
+        data_dict = torch.load(osp.join(self.raw_dir, 'data.pth'))
 
         s = {}
-        N_u = node_nums['user']
-        N_a = node_nums['artist']
-        N_t = node_nums['tag']
+        N_u = 1892
+        N_a = 17632
+        N_t = 1088
+
         s['user'] = (0, N_u)
         s['artist'] = (N_u, N_u + N_a)
         s['tag'] = (N_u + N_a, N_u + N_a + N_t)
 
         print(f'N_u = {N_u}, N_a = {N_a}, N_t = {N_t}')
 
-        # Build the actual edge_index
-        # TODO: Combine edges for different relationships into one
-
-        edge_indices = []
 
         node_offsets = {'user': 0, 'artist': N_u, 'tag': N_u + N_a}
 
-        data['num_nodes'] = N_u + N_a + N_t
+        data['x'] = torch.nn.functional.one_hot(torch.cat((torch.zeros(N_u, dtype=int), torch.ones(N_a, dtype=int), torch.ones(N_t, dtype=int)*2)), num_classes=3).float()
 
-        data.x = torch.nn.functional.one_hot(torch.cat((torch.zeros(N_u, dtype=int), torch.ones(N_a, dtype=int), torch.ones(N_t, dtype=int)*2)), num_classes=3).float()
-        print(data.x)
-
-        A = sp.load_npz(osp.join(self.raw_dir, 'adjM.npz'))
-        for src, dst in product(node_types, node_types):
-            A_sub = A[s[src][0]:s[src][1], s[dst][0]:s[dst][1]].tocoo()
-            if A_sub.nnz > 0:
-                row = torch.from_numpy(A_sub.row).to(torch.long) + node_offsets[src]
-                col = torch.from_numpy(A_sub.col).to(torch.long) + node_offsets[dst]
-                print(f"{src}_{dst}")
-                print(torch.stack([row, col], dim=0))
-                print(torch.max(torch.from_numpy(A_sub.row).to(torch.long)))
-                print(torch.max(torch.from_numpy(A_sub.col).to(torch.long)))
-                edge_indices.append(torch.stack([row, col], dim=0))
-                #if src == 'user' and dst == 'artist':
-                #    user_artist = torch.stack([row, col], dim=0)
-                #data[src, dst].edge_index = torch.stack([row, col], dim=0)
-                #print(f"{src}, {dst}")
-                #print(torch.stack([row, col], dim=0))
-        data['edge_index'] = torch.cat(edge_indices, dim=1)
-        #print(data.edge_index)
+        data['edge_index'] = data_dict['edge_index']
+        data['train_edge_index'] = data_dict['train_edge_index']
+        data['train_edge_label'] = data_dict['train_edge_label']
+        data['val_edge_index'] = data_dict['val_edge_index']
+        data['val_edge_label'] = data_dict['val_edge_label']
+        data['test_edge_index'] = data_dict['test_edge_index']
+        data['test_edge_label'] = data_dict['test_edge_label']
 
 
-        # Compute the train, val and test edges and labels
-        pos_split = np.load(
-            osp.join(self.raw_dir, 'train_val_test_pos_user_artist.npz'))
-        neg_split = np.load(
-            osp.join(self.raw_dir, 'train_val_test_neg_user_artist.npz'))
-
-        for name in ['train', 'val', 'test']:
-            #if name != 'train':
-            edge_index_pos = pos_split[f'{name}_pos_user_artist']
-            edge_index_pos = torch.from_numpy(edge_index_pos)
-            edge_index_pos = edge_index_pos.t().to(torch.long).contiguous()
-            #data['user', 'artist'][f'{name}_pos_edge_index'] = edge_index
-            #else:
-            #    edge_index_pos = user_artist
-
-
-            edge_index_neg = neg_split[f'{name}_neg_user_artist']
-            edge_index_neg = torch.from_numpy(edge_index_neg)
-            edge_index_neg = edge_index_neg.t().to(torch.long).contiguous()
-            
-            edge_index_neg[1,:] = edge_index_neg[1,:] + N_u
-            edge_index_pos[1,:] = edge_index_pos[1,:] + N_u
-            #data['user', 'artist'][f'{name}_neg_edge_index'] = edge_index
-
-            edge_label = torch.cat((torch.ones(edge_index_pos.shape[1], dtype=int), torch.zeros(edge_index_neg.shape[1], dtype=int)))
-            edge_index = torch.cat((edge_index_pos, edge_index_neg), dim=1)
-            print(name)
-            print(edge_index)
-            data[f"{name}_edge_index"] = edge_index
-            data[f"{name}_edge_label"] = edge_label
-
-            #nx_graph = to_networkx(data, node_attrs=["x"])
-            #print([n for n in nx_graph.neighbors(N_u + 836)])
-        
-        #embeddings = np.load('clap_embeddings_17632artists.npy')
-        embeddings = np.load('beats_embedding.npy')
-        print('Loaded embeddings')
-        print(torch.from_numpy(embeddings).shape)
-        print(embeddings)
-        print(N_a)
-        print(data.x.dtype)
-        data['edge_index'] = new_data['edge_index']
-        data['train_edge_index'] = new_data['train_edge_index']
-        data['train_edge_label'] = new_data['train_edge_label']
-        data['val_edge_index'] = new_data['val_edge_index']
-        data['val_edge_label'] = new_data['val_edge_label']
-        data['test_edge_index'] = new_data['test_edge_index']
-        data['test_edge_label'] = new_data['test_edge_label']
-
-        #data_with_embeddings = data.clone()
-        #data_with_embeddings.x = torch.cat((data_with_embeddings.x, torch.cat((torch.zeros((N_u, embeddings.shape[1])), torch.from_numpy(embeddings), torch.zeros((N_t, embeddings.shape[1]))), dim=0)), dim=1).to(torch.float32)
 
         if self.pre_transform is not None:
             data = self.pre_transform(data)
-            #data_with_embeddings = self.pre_transform(data_with_embeddings)
 
         save_data = data
         torch.save(save_data, self.processed_paths[0])
