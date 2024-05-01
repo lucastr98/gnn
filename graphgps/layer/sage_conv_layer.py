e8100e7a4124992d9663f18f3548bcd092acdccf
Luca Strässle <lucastr@student.ethz.ch>
2024-05-01 15:02:38 +0200

    add residual to sage

--- graphgps/layer/sage_conv_layer.py
+++ graphgps/layer/sage_conv_layer.py
@@ -5,18 +5,24 @@ from torch_geometric.nn import SAGEConv
 
 class SAGEConvLayer(nn.Module):
     """GraphSAGE convolution layer."""
-    def __init__(self, dim_in, dim_out, dropout):
+    def __init__(self, dim_in, dim_out, dropout, residual):
         super().__init__()
         self.dim_in = dim_in
         self.dim_out = dim_out
         self.dropout = dropout
+        self.residual = residual
 
         self.model = SAGEConv(dim_in, dim_out, normalize=True, project=True)
 
     def forward(self, batch):
+        x_in = batch.x
+
         batch.x = self.model(batch.x, batch.edge_index)
 
         batch.x = F.elu(batch.x)
         batch.x = F.dropout(batch.x, p=self.dropout, training=self.training)
+
+        if self.residual:
+            batch.x = x_in + batch.x  # residual connection
 
         return batch
